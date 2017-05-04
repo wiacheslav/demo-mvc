adc1fb591daf71649b9db3527e931afd41579877
Viacheslav Yushchenko <viacheslav.yushchenko@privatbank.ua>
2017-05-04 17:35:14 +0300

    first commit

---
+++ src/main/java/com/example/mvc/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.mvc.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
---
+++ src/main/java/com/example/mvc/rest/UserController.java
@@ -0,0 +1,9 @@
+package com.example.mvc.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class UserController {
+}
